459e959af9fe2a3226c0d2eacde8020ab1bde238
ViPhilStar <vipfilstar@gmail.com>
2020-11-02 12:22:17 +0300

    first commit

---
+++ client.js
@@ -0,0 +1,42 @@
+import net from 'net';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'ChatApp> '
+});
+
+const config = {
+  port: 8080
+};
+const clients = net.connect(config, () => {
+  console.log('connected to server!');
+});
+
+clients.on('data', (data) => {
+  process.stdout.clearLine(-1);
+  process.stdout.cursorTo(0);
+  console.log(data.toString());
+  rl.prompt();
+});
+
+clients.on('end', () => {
+  console.log('disconnected from server');
+});
+
+rl.on('line', (line) => {
+  switch (line.trim()) {
+    case 'exit':
+      rl.close();
+      break;
+    default:
+      clients.write(line.trim());
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('You were disconnected!');
+  clients.end();
+  process.exit(0);
+});
---
+++ server.js
@@ -0,0 +1,72 @@
+import net from 'net';
+
+const config = {
+  port: 8080
+};
+
+export class DuplexServer {
+  guestId = 0;
+  connections = new Set();
+
+  constructor(config) {
+    this.port = config.port;
+    net
+      .createServer((socket) => {
+        this.onConnect(socket);
+        this.onNewMessage(socket);
+        this.onDisconnect(socket);
+      })
+      .on('close', () => {
+        console.log('server stop on port: ', this.port);
+      })
+      .on('error', () => {
+        console.error('server error');
+      })
+      .listen(this.port, () => {
+        console.log('server start on port: ', this.port);
+      });
+  }
+
+  onConnect(socket) {
+    this.guestId++;
+    socket.nickname = `User${this.guestId}`;
+    this.connections.add(socket);
+
+    this.sendMessage(socket, `Welcome to telnet chat!`);
+    this.sendBroadcastMessage(socket, `${socket.nickname} joined this chat.`);
+  }
+
+  onDisconnect(socket) {
+    socket.on('end', () => {
+      this.connections.delete(socket);
+      this.sendBroadcastMessage(socket, `${socket.nickname} left this chat!`);
+    });
+  }
+
+  onNewMessage(socket) {
+    socket.on('data', (data) => {
+      const parsedData = data.toString().replace(/\n$/, '');
+      const message = `${socket.nickname} ${parsedData}`;
+      this.sendBroadcastMessage(socket, message);
+    });
+  }
+
+  sendBroadcastMessage(socketFrom, message) {
+    if (this.connections.size === 0) {
+      console.log('Everyone left the chat');
+      return;
+    }
+
+    console.log(message);
+    this.connections.forEach(function (socket) {
+      if (socket.nickname === socketFrom.nickname) return;
+      this.sendMessage(socket, message);
+    }, this);
+  }
+
+  sendMessage(socket, message) {
+    socket.write(message);
+  }
+}
+
+new DuplexServer(config);
